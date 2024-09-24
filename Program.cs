85a335038585fbb064796400fd5fedba76363b45
Vulnorn <mixadrbk31@gmail.com>
2024-09-24 09:50:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NewC_2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string masege;
+            int numberRepetitions;
+
+            Console.Write($"Введите сообщение: ");
+            masege = Console.ReadLine();
+
+            Console.WriteLine( $"Введите ко-во повторений для вывода сообщения: " );
+            numberRepetitions = Convert.ToInt32( Console.ReadLine() );
+
+            while( numberRepetitions > 0 )
+            {
+                Console.WriteLine(masege);
+                numberRepetitions--;
+            }
+        }
+    }
+}
